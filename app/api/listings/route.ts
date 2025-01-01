6165366c57280d96bc02bb80e511d27fad1cadbb
Tochukwu <nzewitochukwu26@gmail.com>
2025-01-01 14:48:31 +0100

    Listing creation step 5 (Description and Price creation POST route)

---
+++ app/api/listings/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(
+    request: Request
+) {
+    const CurrentUser = await getCurrentUser();
+
+    if (!CurrentUser) {
+        return NextResponse.error();
+    }
+
+    const body = await request.json();
+    const {
+        title,
+        description,
+        imageSrc,
+        category,
+        roomCount,
+        bathroomCount,
+        guestCount,
+        location,
+        price
+    } = body;
+
+    const listing = await prisma.listing.create({
+        data: {
+            title,
+            description,
+            imageSrc,
+            category,
+            roomCount,
+            bathroomCount,
+            guestCount,
+            locationValue: location.value,
+            price: parseInt(price, 10),
+            userId: CurrentUser.id
+        }
+    })
+    return NextResponse.json(listing);
+}
